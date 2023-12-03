51df9f13af544601ab0e7b3b7d3b3c3a19f96621
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-03 09:17:57 +0300

    feat: add SQL currency

---
+++ src/main/resources/data/init/create_table_1_currency.sql
@@ -0,0 +1,10 @@
+-- devise to set on a user account (one per account)
+create table if not exists "currency" (
+    id bigserial primary key,
+    name varchar(100) not null,
+    country text not null unique
+);
+
+insert into "currency" (name, country) values ('MGA', 'Madagascar') on conflict (country) do update set name = 'MGA';
+insert into "currency" (name, country) values ('EUR', 'France') on conflict (country) do update set name = 'EUR';
+insert into "currency" (name, country) values ('USD', 'Etats-Unis') on conflict (country) do update set name = 'USD';
