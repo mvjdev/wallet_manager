eabe1767e775dd5122189dbd3665ec4b75b59aa3
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-03 17:18:57 +0300

    feat: add currency reference

--- src/main/resources/data/init/create_table_3_account.sql
+++ src/main/resources/data/init/create_table_3_account.sql
@@ -7,29 +7,33 @@ create table if not exists "account" (
     type varchar(255),
     account_number varchar, -- the bank account number series; example: 0212 2134 1231 4588
 
+    currency_id bigint references "currency"(id) not null,
     creation_timestamp timestamp default now()
 );
 
 insert into "account"
-(id, name, current_amount)
-values (1, 'BMF', 100)
+(id, name, current_amount, currency_id)
+values (1, 'BMF', 100, 1)
 on conflict (id)
     do update
     set name = 'BMF',
+        currency_id = 1,
         current_amount = current_amount + 100;
 
 insert into "account"
-(id, name, current_amount)
-values (2, 'BMOI', 20000000)
+(id, name, current_amount, currency_id)
+values (2, 'BMOI', 20000000, 2)
 on conflict (id)
     do update
     set name = 'BMOI',
+        currency_id = 2,
         current_amount = current_amount + 20000000;
 
 insert into "account"
-(id, name, current_amount)
-values (3, 'US. Bank', 1000)
+(id, name, current_amount, currency_id)
+values (3, 'US. Bank', 1000, 3)
 on conflict (id)
     do update
     set name = 'US. Bank',
+        currency_id = 3,
         current_amount = current_amount + 1000;
