775ac15969d8988583ef7711a36a9743514f231e
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-07 16:49:07 +0300

    feat: transaction type transfer to spend

--- src/main/resources/data/init/create_table_5_transaction.sql
+++ src/main/resources/data/init/create_table_5_transaction.sql
@@ -23,7 +23,7 @@ on conflict (id)
     do update
     set id = 1,
         tag_id = 1,
-        amount = 300,
+        amount = "transaction".amount + 300,
         type = 'spend'::transaction_type,
         transfer_to = null,
         account_id = 1
@@ -35,20 +35,20 @@ on conflict (id)
     do update
     set id = 2,
         tag_id = 3,
-        amount = 2000,
+        amount = "transaction".amount + 2000,
         type = 'claim'::transaction_type,
         transfer_to = null,
         account_id = 2
 ;
 
 insert into "transaction" (id, tag_id, amount, type, transfer_to, account_id)
-values (3, 2, 400, 'transfer'::transaction_type, 1, 3)
+values (3, 2, 400, 'spend'::transaction_type, 1, 3) -- type spend but do a trasnfer
 on conflict (id)
     do update
     set id = 3,
         tag_id = 2,
-        amount = 400,
-        type = 'transfer'::transaction_type,
+        amount = "transaction".amount - 400,
+        type = 'spend'::transaction_type,
         transfer_to = 1,
         account_id = 3
 ;
