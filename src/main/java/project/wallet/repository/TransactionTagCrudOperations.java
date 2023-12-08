ea9324070ff1345a55fe0b15100b43d3ae9d435d
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-08 13:01:53 +0300

    reformat using MakerCrudOperations

--- src/main/java/project/wallet/repository/TransactionTagCrudOperations.java
+++ src/main/java/project/wallet/repository/TransactionTagCrudOperations.java
@@ -1,60 +1,46 @@
 package project.wallet.repository;
 
-import project.wallet.configs.DbConnect;
 import project.wallet.models.TransactionTag;
-import project.wallet.repository.utils.TransactionTagCrudUtils;
 
 import java.sql.*;
-import java.util.*;
+import java.util.List;
 
-public class TransactionTagCrudOperations extends DbConnect implements CrudOperations<TransactionTag> {
-  @Override
-  public TransactionTag save(TransactionTag value) {
+public class TransactionTagCrudOperations implements CrudOperations<TransactionTag> {
+
+  private final MakerCrudOperations<TransactionTag> operations;
+
+  public TransactionTagCrudOperations(){
+    this.operations = new MakerCrudOperations<>("transaction_tag", "id");
+    operations
+      .setInsertColumns(new String[]{"name"})
+      .setFindParser(new String[]{"id", "name"}, this::parseFound);
+    ;
+  }
+
+  private TransactionTag parseFound(ResultSet resultSet) {
     try {
-      PreparedStatement statement = CONNECTION.prepareStatement(TransactionTagCrudUtils.SAVE);
-      statement.setString(1, value.getName());
-      ResultSet result = statement.executeQuery();
-
-      if(result.next()) return TransactionTagCrudUtils.parseFindingTag(result);
-    }catch (SQLException e){
+      return new TransactionTag()
+        .setId(resultSet.getLong("id"))
+        .setName(resultSet.getString("name"))
+      ;
+    } catch (SQLException e) {
       throw new RuntimeException(e);
     }
-    return null;
+  }
+
+
+  @Override
+  public TransactionTag save(TransactionTag value) {
+    return operations.insertValue("'" + value.getName() + "'");
   }
 
   @Override
   public List<TransactionTag> findAll() {
-    List<TransactionTag> tags = new ArrayList<>();
-    try {
-      ResultSet result = CONNECTION
-        .prepareStatement(TransactionTagCrudUtils.FIND_ALL)
-        .executeQuery();
-
-      while (result.next()) tags.add(
-        TransactionTagCrudUtils.parseFindingTag(result)
-      );
-    }catch (SQLException e){
-      throw new RuntimeException(e);
-    }
-    return tags;
+    return operations.findAll();
   }
 
   @Override
-  public TransactionTag delete(TransactionTag value) {
-    try {
-      Long id = value.getId();
-      Objects.requireNonNull(id);
-      TransactionTag found = TransactionTagCrudUtils.findOne(id, CONNECTION);
-
-      PreparedStatement statement = CONNECTION.prepareStatement(TransactionTagCrudUtils.DELETE);
-      statement.setLong(1, id);
-      int deleted = statement.executeUpdate();
-
-      if(deleted > 0 && found != null)
-        return found;
-    }catch (SQLException e){
-      throw new RuntimeException(e);
-    }
-    return null;
+  public TransactionTag deleteById(Long id) {
+    return operations.deleteById(id);
   }
 }
