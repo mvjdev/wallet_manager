88ac15f26382d5218013ea6175b79590681aa64b
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-08 12:29:05 +0300

    feat: transfer tag

--- src/main/resources/data/init/create_table_3_transaction_tag.sql
+++ src/main/resources/data/init/create_table_3_transaction_tag.sql
@@ -6,3 +6,4 @@ create table if not exists "transaction_tag" (
 insert into "transaction_tag" (id, name) values (1, 'food') on conflict (id) do update set name = 'food';
 insert into "transaction_tag" (id, name) values (2, 'electronics') on conflict (id) do update set name = 'electronics';
 insert into "transaction_tag" (id, name) values (3, 'salary') on conflict (id) do update set name = 'salary';
+insert into "transaction_tag" (id, name) values (4, 'transfer') on conflict (id) do update set name = 'transfer';
