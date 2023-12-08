067e4daba964192a14546ccff6fe0ffde30a4d16
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-08 13:36:03 +0300

    feat: accountAmount

---
+++ src/main/java/project/wallet/models/AccountAmount.java
@@ -0,0 +1,33 @@
+package project.wallet.models;
+
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+public class AccountAmount {
+  private Long id;
+  private Double amount;
+  private Account account;
+  private Instant transactionTime;
+
+  public AccountAmount setAccount(Account account) {
+    this.account = account;
+    return this;
+  }
+
+  public AccountAmount setId(Long id) {
+    this.id = id;
+    return this;
+  }
+
+  public AccountAmount setAmount(Double amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  public AccountAmount setTransactionTime(Instant transactionTime) {
+    this.transactionTime = transactionTime;
+    return this;
+  }
+}
