03a0abcdb8b771ed556e336e624ae3f3749b47f3
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-08 13:01:53 +0300

    reformat using MakerCrudOperations

--- src/main/java/project/wallet/repository/TransactionCrudOperations.java
+++ src/main/java/project/wallet/repository/TransactionCrudOperations.java
@@ -1,50 +1,102 @@
 package project.wallet.repository;
 
-import project.wallet.configs.DbConnect;
-import project.wallet.models.Transaction;
-import project.wallet.repository.utils.TransactionCrudUtils;
+import project.wallet.models.*;
 
 import java.sql.*;
+import java.time.Instant;
 import java.util.*;
 
-public class TransactionCrudOperations extends DbConnect implements CrudOperations<Transaction> {
+public class TransactionCrudOperations implements CrudOperations<Transaction> {
+  private final MakerCrudOperations<Transaction> operations;
+
+  public TransactionCrudOperations(){
+    this.operations = new MakerCrudOperations<>("transaction", "id");
+    String[] insertionColumn = {"tag_id", "amount", "type", "account_id"};
+    String[] selectionColumns = {
+      "transaction.id as transaction_id",
+      "tag_id",
+      "amount",
+      "transaction.type as transaction_type",
+      "transfer_to",
+      "transaction.creation_timestamp as transaction_time",
+      "a.id as account_id",
+      "name as account_name",
+      "a.type as account_type",
+      "account_number",
+      "currency_id",
+      "a.creation_timestamp as account_time"
+    };
+
+    operations
+      .setInsertColumns(insertionColumn)
+      .setFindingJoining("full join account a on a.id = transaction.account_id")
+      .setFindParser(selectionColumns, this::parseFound);
+    ;
+  }
+
+  private Transaction parseFound(ResultSet resultSet) {
+    try {
+      TransactionType transactionType = TransactionType.CLAIM;
+      String typeInPSQL = resultSet.getString("transaction_type");
+      if(typeInPSQL != null){
+        transactionType = TransactionType.valueOf(typeInPSQL.toUpperCase());
+      }
+
+      Instant accountTimes = null;
+      Timestamp accountTimestamp = resultSet.getTimestamp("account_time");
+      if(accountTimestamp != null){
+        accountTimes = accountTimestamp.toInstant();
+      }
+      Account account = new Account()
+        .setId(resultSet.getLong("account_id"))
+        .setName(resultSet.getString("account_name"))
+        .setType(resultSet.getString("account_type"))
+        .setAccountNumber(resultSet.getString("account_number"))
+        .setCreationTimestamp(accountTimes)
+      ;
+
+      TransactionTag tag = new TransactionTag();
+
+      Instant transactionInstant = null;
+      Timestamp transactionTimestamp = resultSet.getTimestamp("transaction_time");
+      if(transactionTimestamp != null){
+        transactionInstant = transactionTimestamp.toInstant();
+      }
+
+      return new Transaction()
+        .setId(resultSet.getLong("transaction_id"))
+        .setAmount(resultSet.getDouble("amount"))
+        .setType(transactionType)
+        .setTagId(tag)
+        .setAccountId(account)
+        .setCreationTimestamp(transactionInstant)
+      ;
+    }catch (SQLException e){
+      throw new RuntimeException(e);
+    }
+  }
+
   @Override
   public Transaction save(Transaction value) {
-    return TransactionCrudUtils.complicatedSaving(value, CONNECTION);
+    Account account = value.getAccountId();
+    Objects.requireNonNull(account);
+
+    String values = (
+      value.getId() + ", " +
+      value.getAmount() + ", '" +
+      value.getType().name().toLowerCase() + "', " +
+      account.getId()
+    );
+    return operations.insertValue(values);
   }
 
   @Override
   public List<Transaction> findAll() {
-    List<Transaction> transactions = new ArrayList<>();
-    try {
-      ResultSet resultSet = CONNECTION
-        .prepareStatement(TransactionCrudUtils.FIND_ALL + ";")
-        .executeQuery();
-      while (resultSet.next()){
-        transactions.add(TransactionCrudUtils.parseFinding(resultSet));
-      }
-    }catch (SQLException e){
-      throw new RuntimeException(e);
-    }
-    return transactions;
+    return operations.findAll();
   }
 
   @Override
-  public Transaction delete(Transaction value) {
-    try {
-      Long id = value.getId();
-      Objects.requireNonNull(id);
-      Transaction found = TransactionCrudUtils.findOne(id, CONNECTION);
-
-      PreparedStatement statement = CONNECTION.prepareStatement(TransactionCrudUtils.DELETE);
-      statement.setLong(1, id);
-      int deleted = statement.executeUpdate();
-
-      if(deleted > 0 && found != null)
-        return found;
-    }catch (SQLException e){
-      throw new RuntimeException(e);
-    }
-    return null;
+  public Transaction deleteById(Long id) {
+    return operations.deleteById(id);
   }
 }
