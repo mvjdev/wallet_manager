da4e842dde606d8fdfc040b4bed726ca4398d543
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-14 21:07:30 +0300

    update account

--- src/main/resources/data/migrations/create_table_2_account.sql
+++ src/main/resources/data/migrations/create_table_2_account.sql
@@ -10,30 +10,3 @@ create table if not exists "account" (
     currency_id bigint references "currency"(id) on delete cascade not null,
     creation_timestamp timestamp default now()
 );
-
-insert into "account"
-(id, name, current_amount, currency_id)
-values (1, 'BMF', 100, 1)
-on conflict (id)
-    do update
-    set name = 'BMF',
-        currency_id = 1,
-        current_amount = "account".current_amount + 100;
-
-insert into "account"
-(id, name, current_amount, currency_id)
-values (2, 'BMOI', 20000000, 2)
-on conflict (id)
-    do update
-    set name = 'BMOI',
-        currency_id = 2,
-        current_amount = "account".current_amount + 20000000;
-
-insert into "account"
-(id, name, current_amount, currency_id)
-values (3, 'US. Bank', 1000, 3)
-on conflict (id)
-    do update
-    set name = 'US. Bank',
-        currency_id = 3,
-        current_amount = "account".current_amount + 1000;
