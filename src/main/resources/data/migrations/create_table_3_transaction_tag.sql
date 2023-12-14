6310a4d87df96bbac22a38361a8c431eea4aa055
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-14 21:08:13 +0300

    update transaction_tag

--- src/main/resources/data/migrations/create_table_3_transaction_tag.sql
+++ src/main/resources/data/migrations/create_table_3_transaction_tag.sql
@@ -2,8 +2,3 @@ create table if not exists "transaction_tag" (
     id serial primary key,
     name text unique -- name like: foods, electronics, salaries
 );
-
-insert into "transaction_tag" (id, name) values (1, 'food') on conflict (id) do update set name = 'food';
-insert into "transaction_tag" (id, name) values (2, 'electronics') on conflict (id) do update set name = 'electronics';
-insert into "transaction_tag" (id, name) values (3, 'salary') on conflict (id) do update set name = 'salary';
-insert into "transaction_tag" (id, name) values (4, 'transfer') on conflict (id) do update set name = 'transfer';
