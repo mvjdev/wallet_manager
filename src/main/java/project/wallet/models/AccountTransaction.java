016cbb9e2562874c4d592df2003d233d933b1801
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 16:35:26 +0300

    feat: toString & equalsAndHashCode

--- src/main/java/project/wallet/models/AccountTransaction.java
+++ src/main/java/project/wallet/models/AccountTransaction.java
@@ -1,12 +1,16 @@
 package project.wallet.models;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Builder
+@EqualsAndHashCode
+@ToString
 public class AccountTransaction {
   private Long accountTransactionId;
   private String name;
--- src/main/java/project/wallet/models/Transfer.java
+++ src/main/java/project/wallet/models/Transfer.java
@@ -1,12 +1,16 @@
 package project.wallet.models;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.sql.Timestamp;
 
 @Getter
 @Builder
+@EqualsAndHashCode
+@ToString
 public class Transfer {
     private Account sourceAccount;
     private Account targetAccount;
