5ef4485d7b0cf379f6e782c80fcad2e0362b2b62
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 16:46:30 +0300

    feat: add methods

---
+++ src/main/java/project/wallet/repository/crud/CrudOperations.java
@@ -0,0 +1,14 @@
+package project.wallet.repository.crud;
+
+import java.util.*;
+
+public interface CrudOperations<T> {
+    public T save(T value);
+    public T saveOrUpdate(T value);
+    public List<T> saveAll(List<T> values);
+    public <Id> T findByIdentity(Id identity);
+    public List<T> findAll();
+    public T updateByIdentity(T newValue);
+    public T deleteByIdentity(T value);
+    public List<T> deleteAll();
+}
