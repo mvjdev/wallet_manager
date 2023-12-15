d286b3d9c080e4617052096e6c490ececfc01966
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 17:31:11 +0300

    refactor: move file

--- src/test/java/crud/AccountCrudTest.java
+++ src/test/java/crud/AccountCrudTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 import project.wallet.models.*;
-import project.wallet.repository.AccountCrudOperations;
+import project.wallet.repository.crud.AccountCrudOperations;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,31 +14,37 @@ public class AccountCrudTest {
 
     assertNotNull(accounts);
     assertFalse(accounts.isEmpty());
-    assertNotNull(accounts.get(0).getId());
+    assertNotNull(accounts.get(0).getIdCurrency());
   }
 
   @Test
   public void testSave(){
-    Currency currency = new Currency().setId(3L);
-    Account account = new Account()
-      .setName("jupiter")
-      .setType("bank")
-      .setCurrencyId(currency)
-      .setAccountNumber("2022 0000 1111 0000")
+    Currency currency = new Currency();
+    currency.setCurrencyId(3L);
+
+    Account account = Account.builder()
+      .name("jupiter")
+      .type("bank")
+      .idCurrency(currency)
+      .accountNumber("2022 0000 1111 0000")
+      .build()
     ;
     Account saved = operations.save(account);
 
     assertNotNull(saved);
-    assertNotNull(saved.getId());
+    assertNotNull(saved.getAccountId());
     assertEquals(account.getName(), saved.getName());
   }
 
   @Test
   public void testDelete(){
-    Account deleted = operations.deleteById(2L);
+    Account account = new Account();
+    account.setAccountId(2L);
+
+    Account deleted = operations.deleteByIdentity(account);
 
     assertNotNull(deleted);
-    assertNotNull(deleted.getId());
-    assertEquals(2L, deleted.getId());
+    assertNotNull(deleted.getName());
+    assertEquals(2L, deleted.getAccountId());
   }
 }
