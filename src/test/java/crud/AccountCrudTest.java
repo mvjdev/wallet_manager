c1049b9509325e5300f2d8a18ab572e0f8cc2da4
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 17:54:47 +0300

    feat: save test

--- src/test/java/crud/AccountCrudTest.java
+++ src/test/java/crud/AccountCrudTest.java
@@ -1,50 +1,34 @@
-import org.junit.jupiter.api.Test;
-import project.wallet.models.*;
-import project.wallet.repository.crud.AccountCrudOperations;
-import java.util.List;
+package crud;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+import project.wallet.models.Account;
+import project.wallet.repository.crud.Crud;
+
+
 public class AccountCrudTest {
-  private final AccountCrudOperations operations = new AccountCrudOperations();
-
   @Test
-  public void testFindAll(){
-    List<Account> accounts = operations.findAll();
-
-    assertNotNull(accounts);
-    assertFalse(accounts.isEmpty());
-    assertNotNull(accounts.get(0).getIdCurrency());
-  }
-
-  @Test
-  public void testSave(){
-    Currency currency = new Currency();
-    currency.setCurrencyId(3L);
-
-    Account account = Account.builder()
-      .name("jupiter")
-      .type("bank")
-      .idCurrency(currency)
-      .accountNumber("2022 0000 1111 0000")
-      .build()
-    ;
-    Account saved = operations.save(account);
+  public void testSaveAccount(){
+    Account account = Account
+        .builder()
+        .name("BMI")
+        .type("bank")
+        .currentAmount(2000.0)
+        .idCurrency(Crud.CURRENCY.findByIdentity(2L))
+        .build();
+    Account saved = Crud.ACCOUNT.saveOrUpdate(account);
 
     assertNotNull(saved);
-    assertNotNull(saved.getAccountId());
-    assertEquals(account.getName(), saved.getName());
-  }
-
-  @Test
-  public void testDelete(){
-    Account account = new Account();
-    account.setAccountId(2L);
-
-    Account deleted = operations.deleteByIdentity(account);
-
-    assertNotNull(deleted);
-    assertNotNull(deleted.getName());
-    assertEquals(2L, deleted.getAccountId());
+    assertEquals(
+        account,
+        Account
+            .builder()
+            .name(saved.getName())
+            .type(saved.getType())
+            .currentAmount(saved.getCurrentAmount())
+            .idCurrency(Crud.CURRENCY.findByIdentity(2L))
+            .build()
+    );
   }
 }
