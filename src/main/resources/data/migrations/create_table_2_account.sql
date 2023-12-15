64fbca059c1802c1fe765b99504cb31719cf5bd2
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 17:47:53 +0300

    fix: wrong foreign key

--- src/main/resources/data/migrations/create_table_2_account.sql
+++ src/main/resources/data/migrations/create_table_2_account.sql
@@ -5,8 +5,8 @@ create table if not exists "account" (
     current_amount double precision default 0,
 
     type varchar(255),
-    account_number varchar, -- the bank account number series; example: 0212 2134 1231 4588
+    account_number varchar(255), -- the bank account number series; example: 0212 2134 1231 4588
 
-    id_currency bigint references "currency"(account_id) on delete cascade not null,
+    id_currency bigint references "currency"(currency_id) on delete cascade not null,
     creation_timestamp timestamp default now()
 );
