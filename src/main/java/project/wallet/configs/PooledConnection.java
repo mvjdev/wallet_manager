5c6f3f78bbea66726c97cf96072f754a0aeac1a7
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 16:11:00 +0300

    feat: implement pool connection

--- src/main/java/project/wallet/configs/PooledConnection.java
+++ src/main/java/project/wallet/configs/PooledConnection.java
@@ -1,18 +1,63 @@
 package project.wallet.configs;
 
-import lombok.Getter;
-import lombok.Setter;
 
-import java.sql.Connection;
+import lombok.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
-public class PooledConnection {
-  private Connection temp1;
-  private Connection temp2;
-  private Connection temp3;
-  private Connection temp4;
-  private Connection temp5;
-  private Connection temp6;
-  private Connection temp7;
+@NoArgsConstructor
+public class PooledConnection implements ConnectionPool {
+  private String url;
+  private String user;
+  private String password;
+
+  private List<Connection> connectionPool;
+  private List<Connection> usedConnections = new ArrayList<>();
+  private static int INITIAL_POOL_SIZE = 10;
+
+  public PooledConnection(String url, String user, String password, List<Connection> pool) {
+    this.url = url;
+    this.user = user;
+    this.password = password;
+    this.connectionPool = pool;
+  }
+
+  public static PooledConnection create(
+      String url, String user,
+      String password) throws SQLException {
+
+    List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
+    for (int i = 0; i < INITIAL_POOL_SIZE; i++) {
+      pool.add(createConnection(url, user, password));
+    }
+    return new PooledConnection(url, user, password, pool);
+  }
+
+  @Override
+  public Connection getConnection() {
+    Connection connection = connectionPool
+        .remove(connectionPool.size() - 1);
+    usedConnections.add(connection);
+    return connection;
+  }
+
+  @Override
+  public boolean releaseConnection(Connection connection) {
+    connectionPool.add(connection);
+    return usedConnections.remove(connection);
+  }
+
+  private static Connection createConnection(
+      String url, String user, String password)
+      throws SQLException {
+    return DriverManager.getConnection(url, user, password);
+  }
+
+  public int getSize() {
+    return connectionPool.size() + usedConnections.size();
+  }
 }
