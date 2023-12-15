06bf72c2958eb3f8fadc283ead7458d7fa54e887
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-12-15 17:37:18 +0300

    feat: add find by identity

--- src/main/java/project/wallet/repository/crud/CrudOperations.java
+++ src/main/java/project/wallet/repository/crud/CrudOperations.java
@@ -8,6 +8,7 @@ public interface CrudOperations<T> {
     public List<T> saveAll(List<T> values);
     public <Id> T findByIdentity(Id identity);
     public List<T> findAll();
+    public <Id> List<T> findByAllIdentity(Id identity);
     public T updateByIdentity(T newValue);
     public T deleteByIdentity(T value);
     public List<T> deleteAll();
