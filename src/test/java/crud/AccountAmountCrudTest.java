ec88244fb09e225dd2db79392c76791a7633f64e
ToavinaMaria <ToavinaMaria>
2024-01-09 10:39:08 +0300

    Feat: Added testAccountAmount

--- src/test/java/crud/AccountAmountCrudTest.java
+++ src/test/java/crud/AccountAmountCrudTest.java
@@ -1,5 +1,15 @@
 package crud;
 
-public class AccountAmountCrudTest {
+import org.junit.jupiter.api.Test;
+import project.wallet.models.AccountAmount;
 
+public class AccountAmountCrudTest {
+    @Test
+    public void testAccountAmount(){
+        AccountAmount accountAmount = AccountAmount
+                .builder()
+                .amount(200.0)
+                .accountAmountId(2L)
+                .build();
+    }
 }
